1f73cc5ba3b79f385cea3f3d7674f5f45e545a55
Sean Fairchild <Sean.Fairchild@bhnetwork.com>
2019-05-21 11:03:41 -0400

    update ring

--- ring/lib/ring.ex
+++ ring/lib/ring.ex
@@ -1,18 +1,31 @@
 defmodule Ring do
-  @moduledoc """
-  Documentation for Ring.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ring.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_process(n) do
+    1..n |> Enum.map(fn _ -> spawn(fn -> loop() end) end)
   end
+
+  def loop() do
+    receive do
+      {:link, link_to} when is_pid(link_to) ->
+        Process.link(link_to)
+        loop()
+      :crash ->
+        1/0
+    end
+  end
+
+  def link_processes(procs) do
+    link_processes(procs, [])
+  end
+
+  def link_processes([proc_1, proc_2 | rest], link_processes) do
+    send(proc_1, {:link, proc_2})
+    link_processes([proc_2 | rest], [proc_1 | link_processes])
+  end
+
+  def link_processes([proc|[]], link_processes) do
+    first_process = link_processes |> List.last
+    send(proc, {:link, first_process})
+    :ok
+  end
+
 end
